b46b51d6d6f6ebfc7c77d9a0fa424d1d0d6f13ed
Stanislav Mosyagin <mosstnslv@gmail.com>
2018-07-19 13:41:22 +0500

    lasttest commit

--- lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
+++ lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
@@ -1,11 +1,14 @@
 package ru.atom.makejar;
 
+import ru.atom.instantiation.Base;
+
 /**
  * Class just to test fat jar packing
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(getHelloWorld());
+
     }
 
     public static String getHelloWorld() {
--- lecture03/src/main/java/ru/atom/exception/HelloWorld.java
+++ lecture03/src/main/java/ru/atom/exception/HelloWorld.java
@@ -3,6 +3,7 @@ package ru.atom.exception;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(getHelloWorld());
+        https://www.youtube.com/watch?v=askCqwd_Xkw&index=3&list=PLrCZzMib1e9pnFbVV3u4s7ki5NTnm7WgT 1:19:20
     }
 
     private static String getHelloWorld() {
